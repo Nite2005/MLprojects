320c07443ad41f2b19830a09796e2b86c7b2453d
Nite2005 <kumarnitesh140905@gmail.com>
2025-01-29 23:00:21 +0530

    model traier.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformation
 from src.mlproject.components.data_transformation import DataTransformationConfig
+from src.mlproject.components.model_trainer import ModelTrainerConfig
+from src.mlproject.components.model_trainer import ModelTrainer
+
 import os
 if __name__ == "__main__":
     logging.info("The execution has started")
@@ -17,8 +20,12 @@ if __name__ == "__main__":
 
         data_transformation_config =  DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_ =  data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        ##Model Training
 
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
     except Exception as e:
         logging.info("CustomException")
         raise CustomException(e,sys)
